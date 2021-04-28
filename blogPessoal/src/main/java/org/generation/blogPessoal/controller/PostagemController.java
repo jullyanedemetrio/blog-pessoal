b4ba964e2a1116db684cbc6dda5666ecbc52116c
Jullyane <jullyanedmt@gmail.com>
2021-04-28 17:35:37 -0300

    Inclusão de endPoints

--- blogPessoal/src/main/java/org/generation/blogPessoal/controller/PostagemController.java
+++ blogPessoal/src/main/java/org/generation/blogPessoal/controller/PostagemController.java
@@ -5,14 +5,19 @@ import java.util.List;
 import org.generation.blogPessoal.model.Postagem;
 import org.generation.blogPessoal.repository.PostagemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController //annotation para indicar que o controller é um rest controller 
 @RequestMapping("/postagens")
 @CrossOrigin("*")
 public class PostagemController {
@@ -37,4 +42,18 @@ public class PostagemController {
 		return ResponseEntity.ok(repository.findAllByTituloContainingIgnoreCase(titulo));
 	}
 	
+	@PostMapping //Annotation para mapear o protocolo post 
+	public ResponseEntity<Postagem> post (@RequestBody Postagem postagem) { //Annotation para indicar que o parâmetro está no corpo da request
+		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(postagem));
+	}
+	
+	@PutMapping //Annotation para mapear o protocolo put
+	public ResponseEntity<Postagem> put (@RequestBody Postagem postagem) {
+		return ResponseEntity.status(HttpStatus.OK).body(repository.save(postagem));
+	}
+	
+	@DeleteMapping("/{id}") //Annotation para mapear o protocolo put
+	public void delete(@PathVariable long id) {
+		repository.deleteById(id);
+	}
 }
